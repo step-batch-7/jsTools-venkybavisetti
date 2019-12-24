34694a1eb2402c86bc044cbba044a0c1c92a9212
venkatesh <58026249+venkybavisetti@users.noreply.github.com>
2019-12-24 12:11:36 +0530

    first feature refactored

--- src/sortLib.js
+++ src/sortLib.js
@@ -15,12 +15,20 @@ const parseUserArgs = function(cmdLineArgs) {
 const loadFileContent = function(userArgs, fileSystem) {
   const fsFileExits = isFileExists.bind(fileSystem);
   if (!userArgs.fileName.every(fsFileExits)) return { error: fileError() };
-  return { content: fileSystem.readFile(userArgs.fileName[0], "utf8") };
+  const content = fileSystem.readFile(userArgs.fileName[0], "utf8");
+  return { content };
 };
 
 const sortFileOnOptions = function(content, sortOptions) {
+  if (!sortOptions.every(isValidOption))
+    return { error: "sort: invalid options" };
   const totalLines = content.split("\n");
   return { output: totalLines.sort().join("\n") };
+};
+
+const isValidOption = function(option) {
+  const options = [];
+  options.includes(option);
 };
 
 const fileError = function() {
@@ -35,8 +43,9 @@ const performSortAction = function(cmdLineArgs, fileSystem) {
   const parsedUserArgs = parseUserArgs(cmdLineArgs);
   let { content, error } = loadFileContent(parsedUserArgs, fileSystem);
   if (error) return { error };
-  const { output } = sortFileOnOptions(content, parsedUserArgs.options);
-  return { output };
+  let sortedFile = sortFileOnOptions(content, parsedUserArgs.options);
+  if (sortedFile.error) return { error: sortedFile.error };
+  return { output: sortedFile.output };
 };
 
 module.exports = {
