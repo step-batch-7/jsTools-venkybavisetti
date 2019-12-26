0325fefaf9eb45aa2572408b559d3f2c45aa053b
venkatesh <58026249+venkybavisetti@users.noreply.github.com>
2019-12-26 12:12:54 +0530

    appTest for fileError

--- sort.js
+++ sort.js
@@ -1,11 +1,12 @@
 const fs = require("fs");
 const sort = require("./src/sortLib.js").performSortAction;
+const { stderr, stdout, stdin } = process;
 
 const main = function(cmdLineArgs) {
   const fileHandlingFuncs = fs;
-  const { output, error } = sort(cmdLineArgs, fileHandlingFuncs);
-  process.stdout.write(output);
-  process.stderr.write(error);
+  const { output, error } = sort(cmdLineArgs, fileHandlingFuncs, stdin);
+  stdout.write(output);
+  stderr.write(error);
 };
 
 main(process.argv.slice(2));
